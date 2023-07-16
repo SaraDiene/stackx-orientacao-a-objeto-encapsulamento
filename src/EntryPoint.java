8e2192145e094f01885b44d26a36c29d4d8be6dc
Sara Abreu <webpro.sara@gmail.com>
2023-07-16 14:58:32 -0300

    Entrega tarefa OO - Encapsulamento

---
+++ src/EntryPoint.java
@@ -0,0 +1,16 @@
+//Importe sua classe aqui
+
+class EntryPoint {
+    public static void main(String[] args) {
+        //Instancie sua classe aqui        
+    Pessoa pessoa1 = new Pessoa();
+
+    pessoa1.primeiroNome("Sara");
+    pessoa1.sobrenome("Abreu");
+    pessoa1.idade(36);
+    pessoa1.sexo("F");
+
+
+    pessoa1.imprimirPessoa();
+}   
+}
